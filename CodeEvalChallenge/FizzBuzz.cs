1b763dc50697be90df461d8bd3b4e93ba8540f60
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-04 15:18:44 -0600

    FizzBuzz and LongestCommonSubsequence (brute force)

---
+++ CodeEvalChallenge/FizzBuzz.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalChallenge
+{
+    class FizzBuzz
+    {
+        //static void Main(string[] args)
+        //{
+        //    if (args.Length <= 0)
+        //    {
+        //        Console.WriteLine("Usage: <input filename>");
+        //        return;
+        //    }
+        //    var inputFileLines = File.ReadLines(args[0]);
+
+        //    bool firstLine = true;
+        //    foreach (var line in inputFileLines)
+        //    {
+        //        if (firstLine)
+        //        { firstLine = false; }
+        //        else
+        //        { Console.WriteLine(string.Empty); }
+
+        //        var param = GetParameters(line);
+
+        //        for (int i = 1; i <= param.NumberCount; i++)
+        //        {
+        //            if (i > 1) Console.Write(" ");
+
+        //            bool isFizz = false;
+        //            bool isBuzz = false;
+
+        //            if (i % param.Divisor0 == 0)
+        //            {
+        //                isFizz = true;
+        //                Console.Write("F");
+        //            }
+
+        //            if (i % param.Divisor1 == 0)
+        //            {
+        //                isBuzz = true;
+        //                Console.Write("B");
+        //            }
+
+        //            if (!isFizz && !isBuzz)
+        //            { Console.Write(i); }
+        //        }
+
+
+        //    }
+
+        //}
+
+        //private static Parameters GetParameters(string line)
+        //{
+        //    var args = line.Split(new[] { ' ' }, 3);
+        //    return new Parameters
+        //    {
+        //        Divisor0 = int.Parse(args[0]),
+        //        Divisor1 = int.Parse(args[1]),
+        //        NumberCount = int.Parse(args[2])
+        //    };
+        //}
+
+        //public struct Parameters
+        //{
+        //    public int Divisor0 { get; set; }
+        //    public int Divisor1 { get; set; }
+        //    public int NumberCount { get; set; }
+        //}
+    }
+}
---
+++ CodeEvalChallenge/LongestCommonSubsequence.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalChallenge
+{
+    class LongestCommonSubsequence
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: <input filename>");
+                return;
+            }
+            var inputFileLines = File.ReadLines(args[0]);
+
+            foreach (var line in inputFileLines)
+            {
+                var param = GetParameters(line);
+
+                var output = FindLcs(param.Seq0, param.Seq1);
+
+                if (!string.IsNullOrWhiteSpace(output))
+                { Console.WriteLine(output); }
+            }
+        }
+
+        private static string FindLcs(string seq0, string seq1)
+        {
+            var output = string.Empty;
+            for (int i = 0; i < seq0.Length; i++)
+            {
+                var cs = FindCommonString(seq0.Substring(i), seq1);
+                if (cs.Length > output.Length)
+                { output = cs; }
+            }
+
+            return output;
+        }
+
+        private static string FindCommonString(string substring, string seq1)
+        {
+            var output = string.Empty;
+            foreach (char t in substring)
+            {
+                var index = seq1.IndexOf(t);
+                if (index > -1)
+                {
+                    output += t;
+                    if (index < seq1.Length)
+                    { seq1 = seq1.Substring(index + 1); }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return output;
+        }
+
+        private static Parameters GetParameters(string line)
+        {
+            var args = line.Split(new[] { ';' }, 2);
+            return new Parameters
+            {
+                Seq0 = args[0],
+                Seq1 = args[1]
+            };
+        }
+
+        public struct Parameters
+        {
+            public string Seq0 { get; set; }
+            public string Seq1 { get; set; }
+        }
+    }
+}
