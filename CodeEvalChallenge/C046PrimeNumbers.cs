b6b5638961d2450e6f4e2bb96c397d7deaae7cd2
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-05 12:27:31 -0600

    Prime Numbers brute force

---
+++ CodeEvalChallenge/C046PrimeNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalChallenge
+{
+    class C046PrimeNumbers
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: <input filename>");
+                return;
+            }
+            var inputFileLines = File.ReadLines(args[0]);
+
+            foreach (var line in inputFileLines.Where(l => !string.IsNullOrWhiteSpace(l)))
+            {
+                var output = GetPrimes(uint.Parse(line));
+
+                if (!string.IsNullOrWhiteSpace(output))
+                {
+                    Console.WriteLine(output);
+                }
+            }
+        }
+
+        private static string GetPrimes(uint seed)
+        {
+            string output = string.Empty;
+            bool first = true;
+            for (uint i = 2; i < seed; i++)
+            {
+                if (IsPrime(i))
+                {
+                    if (first)
+                    {
+                        first = false;
+                        output = i + string.Empty;
+                    }
+                    else
+                    { output += "," + i; }
+                }
+            }
+
+            return output;
+        }
+
+        private static bool IsPrime(uint i)
+        {
+            for (uint j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- CodeEvalChallenge/LongestCommonSubsequence.cs
+++ CodeEvalChallenge/LongestCommonSubsequence.cs
@@ -1,90 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CodeEvalChallenge
-{
-    class LongestCommonSubsequence
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length <= 0)
-            {
-                Console.WriteLine("Usage: <input filename>");
-                return;
-            }
-            var inputFileLines = File.ReadLines(args[0]);
+//namespace CodeEvalChallenge
+//{
+//    class LongestCommonSubsequence
+//    {
+//        static void Main(string[] args)
+//        {
+//            if (args.Length <= 0)
+//            {
+//                Console.WriteLine("Usage: <input filename>");
+//                return;
+//            }
+//            var inputFileLines = File.ReadLines(args[0]);
 
-            foreach (var line in inputFileLines.Where(l => !string.IsNullOrWhiteSpace(l)))
-            {
-                var param = GetParameters(line);
+//            foreach (var line in inputFileLines.Where(l => !string.IsNullOrWhiteSpace(l)))
+//            {
+//                var param = GetParameters(line);
 
-                if (string.IsNullOrWhiteSpace(param.Seq0) || string.IsNullOrWhiteSpace(param.Seq1))
-                {
-                    break;
-                }
+//                if (string.IsNullOrWhiteSpace(param.Seq0) || string.IsNullOrWhiteSpace(param.Seq1))
+//                {
+//                    break;
+//                }
 
-                var output = FindLcs(param.Seq0, param.Seq1);
+//                var output = FindLcs(param.Seq0, param.Seq1);
 
-                if (!string.IsNullOrWhiteSpace(output))
-                {
-                    Console.WriteLine(output);
-                }
-            }
-        }
+//                if (!string.IsNullOrWhiteSpace(output))
+//                {
+//                    Console.WriteLine(output);
+//                }
+//            }
+//        }
 
-        private static string FindLcs(string seq0, string seq1)
-        {
-            var output = string.Empty;
+//        private static string FindLcs(string seq0, string seq1)
+//        {
+//            var output = string.Empty;
 
-            for (int i = 0; i < seq0.Length; i++)
-            {
-                var index = seq1.IndexOf(seq0[i]);
+//            for (int i = 0; i < seq0.Length; i++)
+//            {
+//                var index = seq1.IndexOf(seq0[i]);
 
-                if (index > -1)
-                {
-                    if (i + 1 < seq0.Length && index + 1 < seq1.Length)
-                    {
-                        var lcs = FindLcs(seq0.Substring(i + 1), seq1.Substring(index + 1));
+//                if (index > -1)
+//                {
+//                    if (i + 1 < seq0.Length && index + 1 < seq1.Length)
+//                    {
+//                        var lcs = FindLcs(seq0.Substring(i + 1), seq1.Substring(index + 1));
 
-                        if (output.Length - 1 < lcs.Length)
-                        {
-                            output = seq0[i] + lcs;
-                        }
-                    }
-                    if (output == string.Empty)
-                    {
-                        output = seq0[i] + string.Empty;
-                    }
-                }
-            }
+//                        if (output.Length - 1 < lcs.Length)
+//                        {
+//                            output = seq0[i] + lcs;
+//                        }
+//                    }
+//                    if (output == string.Empty)
+//                    {
+//                        output = seq0[i] + string.Empty;
+//                    }
+//                }
+//            }
 
-            return output;
-        }
+//            return output;
+//        }
 
-        private static Parameters GetParameters(string line)
-        {
-            var args = line.Split(new[] { ';' }, 2);
-            if (args.Length == 2)
-            {
-                return new Parameters
-                {
-                    Seq0 = args[0],
-                    Seq1 = args[1]
-                };
-            }
-            return new Parameters();
-        }
+//        private static Parameters GetParameters(string line)
+//        {
+//            var args = line.Split(new[] { ';' }, 2);
+//            if (args.Length == 2)
+//            {
+//                return new Parameters
+//                {
+//                    Seq0 = args[0],
+//                    Seq1 = args[1]
+//                };
+//            }
+//            return new Parameters();
+//        }
 
-        public struct Parameters
-        {
-            public string Seq0 { get; set; }
+//        public struct Parameters
+//        {
+//            public string Seq0 { get; set; }
 
-            public string Seq1 { get; set; }
-        }
-    }
+//            public string Seq1 { get; set; }
+//        }
+//    }
 
-}
+//}
