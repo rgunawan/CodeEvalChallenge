816a7f09c1b5f288ea5d3f228834f870cff4fbdf
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-05 14:40:43 -0600

    Longest Lines

---
+++ CodeEvalChallenge/C002LongestLines.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodeEvalChallenge
+{
+    public class C002LongestLines
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: <input filename>");
+                return;
+            }
+            var inputFileLines = File.ReadLines(args[0]);
+              
+            bool firstLine = true;
+            int nLines = 0;
+            var output = new SortedSet<string>(new StringLengthComparer());
+
+            foreach (var line in inputFileLines)
+            {
+                if (firstLine)
+                {
+                    nLines = int.Parse(line);
+                    firstLine = false;
+                    continue;
+                }
+
+                output.Add(line);
+            }
+
+            int count = 0;
+            foreach (var line in output)
+            {
+                Console.WriteLine(line);
+                count++;
+                if (count == nLines)
+                    break;
+            }
+        }
+
+        public class StringLengthComparer : IComparer<string>
+        {
+            #region IComparer implementation
+
+            public int Compare(string x, string y)
+            {
+                if (x == y)
+                    return 0;
+                if (x == null)
+                    return 1;
+                if (y == null)
+                    return -1;
+                return y.Length.CompareTo(x.Length);
+            }
+
+            #endregion
+        }
+    }
+}
--- CodeEvalChallenge/C004SumFirst1000PrimeNumbers.cs
+++ CodeEvalChallenge/C004SumFirst1000PrimeNumbers.cs
@@ -1,67 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeEvalChallenge
-{
-    class C004SumFirst1000PrimeNumbers
-    {
-        static void Main(string[] args)
-        {
-            var output = GetPrimes(1000);
-
-            Console.WriteLine(output.Aggregate((i, sum) => sum + i));
-        }
-
-        private static IEnumerable<uint> GetPrimes(uint totalCount)
-        {
-            uint i = 2;
-            int count = 0;
-
-            while (count < totalCount)
-            {
-                if (IsPrime(i))
-                {
-                    LastKnownPrimes.Add(i);
-                    yield return i;
-                    count++;
-                }
-                i++;
-            }
-        }
-
-        private static IList<uint> LastKnownPrimes = new List<uint>();
-
-        static IEnumerable<uint> GettKnownPrimes(uint seed)
-        {
-            if (LastKnownPrimes.Count == 0)
-                yield break;
-
-            for (int i = 0; i < LastKnownPrimes.Count; i++)
-            {
-                if (LastKnownPrimes[i] < seed)
-                {
-                    yield return LastKnownPrimes[i];
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
-        private static bool IsPrime(uint i)
-        {
-            for (int j = 0; j < LastKnownPrimes.Count; j++)
-            {
-                if (i % LastKnownPrimes[j] == 0)
-                    return false;
-            }
-
-            return true;
-        }
-    }
-}
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//
+//namespace CodeEvalChallenge
+//{
+//    class C004SumFirst1000PrimeNumbers
+//    {
+//        static void Main(string[] args)
+//        {
+//            var output = GetPrimes(1000);
+//
+//            Console.WriteLine(output.Aggregate((i, sum) => sum + i));
+//        }
+//
+//        private static IEnumerable<uint> GetPrimes(uint totalCount)
+//        {
+//            uint i = 2;
+//            int count = 0;
+//
+//            while (count < totalCount)
+//            {
+//                if (IsPrime(i))
+//                {
+//                    LastKnownPrimes.Add(i);
+//                    yield return i;
+//                    count++;
+//                }
+//                i++;
+//            }
+//        }
+//
+//        private static IList<uint> LastKnownPrimes = new List<uint>();
+//
+//        static IEnumerable<uint> GettKnownPrimes(uint seed)
+//        {
+//            if (LastKnownPrimes.Count == 0)
+//                yield break;
+//
+//            for (int i = 0; i < LastKnownPrimes.Count; i++)
+//            {
+//                if (LastKnownPrimes[i] < seed)
+//                {
+//                    yield return LastKnownPrimes[i];
+//                }
+//                else
+//                {
+//                    break;
+//                }
+//            }
+//        }
+//
+//        private static bool IsPrime(uint i)
+//        {
+//            for (int j = 0; j < LastKnownPrimes.Count; j++)
+//            {
+//                if (i % LastKnownPrimes[j] == 0)
+//                    return false;
+//            }
+//
+//            return true;
+//        }
+//    }
+//}
