7e411393f2d70d1a1e1b7239c951c3636f173cdd
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-05 11:16:07 -0600

    LongestCommonSubsequence solved.

--- CodeEvalChallenge/LongestCommonSubsequence.cs
+++ CodeEvalChallenge/LongestCommonSubsequence.cs
@@ -18,64 +18,73 @@ namespace CodeEvalChallenge
             }
             var inputFileLines = File.ReadLines(args[0]);
 
-            foreach (var line in inputFileLines)
+            foreach (var line in inputFileLines.Where(l => !string.IsNullOrWhiteSpace(l)))
             {
                 var param = GetParameters(line);
+
+                if (string.IsNullOrWhiteSpace(param.Seq0) || string.IsNullOrWhiteSpace(param.Seq1))
+                {
+                    break;
+                }
 
                 var output = FindLcs(param.Seq0, param.Seq1);
 
                 if (!string.IsNullOrWhiteSpace(output))
-                { Console.WriteLine(output); }
+                {
+                    Console.WriteLine(output);
+                }
             }
         }
 
         private static string FindLcs(string seq0, string seq1)
         {
             var output = string.Empty;
+
             for (int i = 0; i < seq0.Length; i++)
             {
-                var cs = FindCommonString(seq0.Substring(i), seq1);
-                if (cs.Length > output.Length)
-                { output = cs; }
-            }
+                var index = seq1.IndexOf(seq0[i]);
 
-            return output;
-        }
-
-        private static string FindCommonString(string substring, string seq1)
-        {
-            var output = string.Empty;
-            foreach (char t in substring)
-            {
-                var index = seq1.IndexOf(t);
                 if (index > -1)
                 {
-                    output += t;
-                    if (index < seq1.Length)
-                    { seq1 = seq1.Substring(index + 1); }
-                    else
+                    if (i + 1 < seq0.Length && index + 1 < seq1.Length)
                     {
-                        break;
+                        var lcs = FindLcs(seq0.Substring(i + 1), seq1.Substring(index + 1));
+
+                        if (output.Length - 1 < lcs.Length)
+                        {
+                            output = seq0[i] + lcs;
+                        }
+                    }
+                    if (output == string.Empty)
+                    {
+                        output = seq0[i] + string.Empty;
                     }
                 }
             }
+
             return output;
         }
 
         private static Parameters GetParameters(string line)
         {
             var args = line.Split(new[] { ';' }, 2);
-            return new Parameters
+            if (args.Length == 2)
             {
-                Seq0 = args[0],
-                Seq1 = args[1]
-            };
+                return new Parameters
+                {
+                    Seq0 = args[0],
+                    Seq1 = args[1]
+                };
+            }
+            return new Parameters();
         }
 
         public struct Parameters
         {
             public string Seq0 { get; set; }
+
             public string Seq1 { get; set; }
         }
     }
+
 }
