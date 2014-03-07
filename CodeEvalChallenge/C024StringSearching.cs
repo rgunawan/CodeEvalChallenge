db3ba49834bfd89b818e0b4637c4cf41ce97ddd5
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-07 13:02:06 -0600

    String Searching

---
+++ CodeEvalChallenge/C024StringSearching.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CodeEvalChallenge
+{
+    public class C024StringSearching
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                var strings = line.Split(new [] { ',' });
+
+                bool stringFound = Match(strings[0], strings[1]);
+
+                if (stringFound)
+                    Console.WriteLine("true");
+                else
+                    Console.WriteLine("false");
+            }
+        }
+
+        static IEnumerable<string> GetPatterns(string pattern)
+        {
+            var output = new List<string>();
+            var chunk = new List<char>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] == '\\' && pattern.Length > i + 1 && pattern[i + 1] == '*')
+                {
+                    chunk.Add('*');
+                    i++;
+                }
+                else if (pattern[i] == '*')
+                {
+                    if (chunk.Count > 0)
+                    {
+                        output.Add(string.Join("", chunk));
+                        chunk.Clear();
+                    }
+                }
+                else
+                {
+                    chunk.Add(pattern[i]);
+                }                
+            }
+
+            if (chunk.Count > 0)
+            {
+                output.Add(string.Join("", chunk));
+            }
+
+            return output;
+        }
+
+        static bool Match(string str, string toMatch)
+        {
+            var patterns = GetPatterns(toMatch);
+
+            int index = 0;
+            foreach (var pattern in patterns)
+            {
+                int foundIndex = Find(str, index, pattern);
+                if (foundIndex == -1)
+                {
+                    return false;
+                }
+                else
+                {
+                    index = foundIndex + pattern.Length + 1;
+                }
+            }
+
+            return true;
+
+        }
+
+        static int Find(string str, int start, string pattern)
+        {
+            for (int i = start; i < str.Length; i++)
+            {
+                if (str[i] == pattern[0] && str.Length - i >= pattern.Length)
+                {
+                    if (FindExact(str, i, pattern))
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+
+        static bool FindExact(string str, int start, string pattern)
+        {
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (str[start + i] != pattern[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+
--- CodeEvalChallenge/C052TextDollar.cs
+++ CodeEvalChallenge/C052TextDollar.cs
@@ -1,80 +1,80 @@
-﻿using System;
-using System.IO;
-
-namespace CodeEvalChallenge
-{
-    public class C052TextDollar
-    {
-        static void Main(string[] args)
-        {
-            foreach (var line in File.ReadLines(args[0]))
-            {
-                Console.WriteLine(GetText(int.Parse(line)));
-            }
-        }
-
-        static string GetText(int i)
-        {
-            var dollarText = i > 1 ? "Dollars" : "Dollars"; // the spec says always plural ...
-            var output = string.Empty;
-            output += Textify(i, 6, "Million");
-            output += Textify(i, 3, "Thousand");
-            output += TextifyHundreds(i % 1000);
-            return output + dollarText;
-        }
-
-        static string Textify(int i, int minPower, string text)
-        {
-            int lowerLimit = (int)Math.Pow(10, minPower);
-            if (i < (lowerLimit))
-                return string.Empty;
-            int number = (i % (1000 * lowerLimit)) / lowerLimit;
-
-            var numberText = TextifyHundreds(number);
-            if (!string.IsNullOrWhiteSpace(numberText))
-                return TextifyHundreds(number) + text;
-            return string.Empty;
-        }
-
-        static string TextifyHundreds(int i)
-        {
-            if (i < 100)
-                return TextifyTens(i);
-            int index = (i % 1000) / 100;
-            return Ones[index] + "Hundred" + TextifyTens(i);
-        }
-
-        static string TextifyTens(int i)
-        {
-            int tens = i % 100;
-            if (tens >= 20)
-                return TextifyRegularTens(tens);
-            else
-                return TextifyTeens(tens);
-        }
-
-        static string TextifyRegularTens(int tens)
-        {
-            int index = tens / 10;
-            return Tens[index] + TextifyOnes(tens);
-        }
-
-        static string TextifyTeens(int tens)
-        {
-            return Ones[tens];
-        }
-
-        static string TextifyOnes(int i)
-        {
-            int number = i % 10;
-
-            return number == 0
-                    ? string.Empty
-                    : Ones[number];
-        }
-
-        private static string[] Tens = new [] { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-        private static string[] Ones = new [] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-    }
-}
-
+﻿//using System;
+//using System.IO;
+//
+//namespace CodeEvalChallenge
+//{
+//    public class C052TextDollar
+//    {
+//        static void Main(string[] args)
+//        {
+//            foreach (var line in File.ReadLines(args[0]))
+//            {
+//                Console.WriteLine(GetText(int.Parse(line)));
+//            }
+//        }
+//
+//        static string GetText(int i)
+//        {
+//            var dollarText = i > 1 ? "Dollars" : "Dollars"; // the spec says always plural ...
+//            var output = string.Empty;
+//            output += Textify(i, 6, "Million");
+//            output += Textify(i, 3, "Thousand");
+//            output += TextifyHundreds(i % 1000);
+//            return output + dollarText;
+//        }
+//
+//        static string Textify(int i, int minPower, string text)
+//        {
+//            int lowerLimit = (int)Math.Pow(10, minPower);
+//            if (i < (lowerLimit))
+//                return string.Empty;
+//            int number = (i % (1000 * lowerLimit)) / lowerLimit;
+//
+//            var numberText = TextifyHundreds(number);
+//            if (!string.IsNullOrWhiteSpace(numberText))
+//                return TextifyHundreds(number) + text;
+//            return string.Empty;
+//        }
+//
+//        static string TextifyHundreds(int i)
+//        {
+//            if (i < 100)
+//                return TextifyTens(i);
+//            int index = (i % 1000) / 100;
+//            return Ones[index] + "Hundred" + TextifyTens(i);
+//        }
+//
+//        static string TextifyTens(int i)
+//        {
+//            int tens = i % 100;
+//            if (tens >= 20)
+//                return TextifyRegularTens(tens);
+//            else
+//                return TextifyTeens(tens);
+//        }
+//
+//        static string TextifyRegularTens(int tens)
+//        {
+//            int index = tens / 10;
+//            return Tens[index] + TextifyOnes(tens);
+//        }
+//
+//        static string TextifyTeens(int tens)
+//        {
+//            return Ones[tens];
+//        }
+//
+//        static string TextifyOnes(int i)
+//        {
+//            int number = i % 10;
+//
+//            return number == 0
+//                    ? string.Empty
+//                    : Ones[number];
+//        }
+//
+//        private static string[] Tens = new [] { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+//        private static string[] Ones = new [] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+//    }
+//}
+//
