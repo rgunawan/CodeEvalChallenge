7259765a567a98975f215b544ce0cc7507509893
Raymond Gunawan <giveawayyard@yahoo.com>
2014-03-12 11:12:36 -0500

    Clean up

--- CodeEvalChallengeCpp/CodeEvalChallengeCpp/C003PrimePalindrome.cpp
+++ CodeEvalChallengeCpp/CodeEvalChallengeCpp/C003PrimePalindrome.cpp
@@ -5,34 +5,34 @@
 //  Created by Raymond Gunawan on 3/7/14.
 //  Copyright (c) 2014 Raymond Gunawan. All rights reserved.
 //
-//
-//#include <iostream>
-//
-//bool isPrime(int toCheck)
-//{
-//    for (int i = 2; i < toCheck; i++) {
-//        if (toCheck % i ==0) {
-//            return false;
-//        }
-//    }
-//    return true;
-//}
-//
-//bool isPalindrome(int toCheck)
-//{
-//    int hundred = (int) toCheck / 100;
-//    int singles = toCheck % 10;
-//    return hundred == singles;
-//}
-//
-//int main(int argc, const char * argv[])
-//{
-//    for (int i = 1000; i > 0; i--) {
-//        if (isPrime(i) && isPalindrome(i)) {
-//            std::cout << i << std::endl;
-//            break;
-//        }
-//    }
-//    
-//    return 0;
-//}
+
+#include <iostream>
+
+bool isPrime(int toCheck)
+{
+    for (int i = 2; i < toCheck; i++) {
+        if (toCheck % i ==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPalindrome(int toCheck)
+{
+    int hundred = (int) toCheck / 100;
+    int singles = toCheck % 10;
+    return hundred == singles;
+}
+
+int main(int argc, const char * argv[])
+{
+    for (int i = 1000; i > 0; i--) {
+        if (isPrime(i) && isPalindrome(i)) {
+            std::cout << i << std::endl;
+            break;
+        }
+    }
+    
+    return 0;
+}
--- CodeEvalChallengeCpp/CodeEvalChallengeCpp/C008ReverseWords.cpp
+++ CodeEvalChallengeCpp/CodeEvalChallengeCpp/C008ReverseWords.cpp
@@ -6,51 +6,51 @@
 //  Copyright (c) 2014 Raymond Gunawan. All rights reserved.
 //
 //
-//#include <fstream>
-//#include <sstream>
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main (int argc, const char * argv[])
-//{
-//    std::ifstream inputFile;
-//    inputFile.open(argv[1]);
-//    
-//    if (!inputFile.is_open()) {
-//        return 1;
-//    }
-//    
-//    while (!inputFile.eof()) {
-//        string line;
-//        getline(inputFile, line);
-//        
-//        std::istringstream iss(line);
-//        string token;
-//        
-//        vector<string> container;
-//        
-//        while (getline(iss, token, ' '))
-//        {
-//            container.push_back(token);
-//        }
-//        
-//        bool first = true;
-//        for (long i = container.size() -1; i > -1; i--) {
-//            if (first) {
-//                first = false;
-//            } else {
-//                cout << " ";
-//            }
-//            
-//            cout << container.at(i);
-//        }
-//        cout << endl;
-//    }
-//    
-//    
-//    inputFile.close();
-//    
-//    return 0;
-//}
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+    std::ifstream inputFile;
+    inputFile.open(argv[1]);
+    
+    if (!inputFile.is_open()) {
+        return 1;
+    }
+    
+    while (!inputFile.eof()) {
+        string line;
+        getline(inputFile, line);
+        
+        std::istringstream iss(line);
+        string token;
+        
+        vector<string> container;
+        
+        while (getline(iss, token, ' '))
+        {
+            container.push_back(token);
+        }
+        
+        bool first = true;
+        for (long i = container.size() -1; i > -1; i--) {
+            if (first) {
+                first = false;
+            } else {
+                cout << " ";
+            }
+            
+            cout << container.at(i);
+        }
+        cout << endl;
+    }
+    
+    
+    inputFile.close();
+    
+    return 0;
+}
